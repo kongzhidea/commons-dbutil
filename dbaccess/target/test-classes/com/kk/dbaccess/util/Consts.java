cc13d2a61a58ce7de6641e0bd9c72219eae306a9
kongzhidea <563150913@qq.com>
2016-07-27 20:41:07 +0800

    delete target

--- dbaccess/target/test-classes/com/kk/dbaccess/util/Consts.java
+++
@@ -1,9 +0,0 @@
-package com.kk.dbaccess.util;
-
-public class Consts {
-    public static String driverClassName = "com.mysql.jdbc.Driver";
-    public static String url = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&transformedBitIsBoolean=true";
-    public static String username = "root";
-    public static String password = "";
-
-}
